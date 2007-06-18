065be4969b6115da1e7849f30005b39fc43a0c7c
Mark Overmeer <mark@overmeer.net>
2007-06-18 19:31:42 +0000

    Import of MARKOV/OODoc-1.01 from CPAN.
    gitpan-cpan-distribution: OODoc
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         MARKOV/OODoc-1.01.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/OODoc/Parser.pm
+++ lib/OODoc/Parser.pm
@@ -1,11 +1,11 @@
 # Copyrights 2003-2007 by Mark Overmeer.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 1.00.
+# Pod stripped from pm file by OODoc 1.01.
 
 package OODoc::Parser;
 use vars '$VERSION';
-$VERSION = '1.00';
+$VERSION = '1.01';
 use base 'OODoc::Object';
 
 use strict;
